604300c7588f623955c5531bbfde4b2a064501e9
auguscj <131878707+auguscj@users.noreply.github.com>
2025-01-27 22:35:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,8 @@ import logging
 logging.basicConfig(filename='coin.log', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 bad_website_suffix = ['vip','top','teach','net']
+
+bad_domains = ['twitter.com', 'x.com',
+               'tiktok.com', 'facebook.com',
+               'solana.com', "instagram.com",
+               "youtube.com",'wechat.com','yahoo.com','kick.com','baidu.com','deepseek.com']
