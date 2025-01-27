9230279a8a00b895c2876248222becdf523bc887
auguscj <131878707+auguscj@users.noreply.github.com>
2025-01-27 22:35:09 +0800

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,28 +1,80 @@
 import requests
-from config import coin_detail_url,coin_creator,logging,bad_website_suffix
+from config import coin_detail_url,coin_creator,logging,bad_website_suffix,bad_domains
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 from collections import deque
-
-
-def base_filter(twitter,website):
-    #过滤双空网站
-    if twitter=="none" and website=="none":
-        return False
-
-    #过滤一些后缀的网站
-    website = website_format(website)
-    domain_end = website.split(".")[-1]
-    if domain_end in bad_website_suffix:
-        return False
-
-    return True
+import sqlite3
+from urllib.parse import urlparse
+from collections import Counter
 
 def website_format(website):
     website = website.replace("https://", "")
     website = website.replace("http://", "")
     clean_domain = website.split('/')[0] # final format like www.xx.com xx.io ....
     return clean_domain
+
+
+def base_filter(twitter,website,duplicate_domains):
+    #过滤双空网站
+    if twitter=="None" and website=="None":
+        return False
+
+    #过滤一些后缀的网站
+    website = website_format(website)
+    if website in duplicate_domains:
+        return False
+
+    domain_end = website.split(".")[-1]
+    if domain_end in bad_website_suffix:
+        return False
+
+    # 过滤非顶级一级二级域名
+    if website.count('.') > 2:
+        return False
+
+    # 过滤掉一些著名网站
+    for bad_domain in bad_domains:
+        if bad_domain in website:
+            return False
+
+    return True
+
+# conn = sqlite3.connect('coins_duplicate.db')
+# cursor = conn.cursor()
+# sql = "SELECT twitter,website FROM coins WHERE (twitter, website) IN (SELECT twitter, website FROM coins GROUP BY twitter, website HAVING COUNT(*) = 1);"
+# cursor.execute(sql)
+# result = cursor.fetchall()
+# for i in result:
+#     print(i)
+
+def find_duplicate_domain():
+    conn = sqlite3.connect('coins_duplicate.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT website FROM coins')
+    websites = cursor.fetchall()
+
+    # 提取域名部分并统计出现次数
+    domain_counts = Counter(urlparse(website[0]).netloc for website in websites)
+
+    # 筛选出出现次数大于1的域名
+    duplicate_domains = {domain for domain, count in domain_counts.items() if count > 1}
+    return duplicate_domains
+
+# duplicate_domains = find_duplicate_domain()
+# final_resul = []
+# for i in result:
+#     twitter = i[0]
+#     website = i[1]
+#     if base_filter(twitter,website):
+#         website = website_format(website)
+#         if website not in duplicate_domains:
+#             final_resul.append(website)
+#
+# print(len(final_resul))
+
+
+# print(final_resul)
+
 
 
 def coin_filter(mint):
@@ -71,7 +123,6 @@ def coin_filter(mint):
 
 
 def filter_website(website):
-    bad_domains = ['twitter.com', 'x.com', 'tiktok.com', 'facebook.com', 'github.com', "instagram.com", "youtube.com"]
     # 判断是否是io或者com域名
     website = website.replace("https://", "")
     website = website.replace("http://", "")
